79597ecc217770ce0e1f48ff80ebe8422ce54712
AlejoFerrari463 <alejoferrari463@gmail.com>
2024-05-15 14:17:57 -0300

    organizando el javascript

---
+++ javascript/cargaPagina.js
@@ -0,0 +1,136 @@
+export function cargarData(page){
+
+
+    if (page==1){
+        const back = document.querySelector(".back")
+        back.style.display="none"
+    }
+    else {
+        const back = document.querySelector(".back")
+        back.style.display="flex"
+    }
+    
+    if (page==6){
+        const next = document.querySelector(".next")
+        next.style.display="none"
+    }
+    else {
+        const next = document.querySelector(".next")
+        next.style.display="flex"
+    }
+    
+        
+    
+    const api = `https://dragonball-api.com/api/characters?page=${page}&limit=10`
+    
+    
+    const main = document.querySelector("#main")
+    
+    main.innerHTML=`
+        
+    
+    `
+    
+    const nube = document.querySelector(".page-nube")
+    nube.innerText=`${page}`
+    
+    
+    
+    
+    
+    fetch(api)
+        .then((info)=>{
+            return info.json()
+            
+        })
+        .then((response)=>{
+    
+            response.items.forEach(element => {
+                const {id} = element
+    
+                fetch(`https://dragonball-api.com/api/characters/${id}`)
+                
+                .then((info2)=>{
+                    return info2.json()
+                })
+                .then((response2)=>{
+                    
+                    const {name, image, race} = response2
+    
+                    const main = document.querySelector("#main")
+    
+                    const col = document.createElement("div")
+                    col.classList.add("col")
+    
+                    const cards = document.createElement("div")
+                    cards.classList.add("card")
+                    const imagen = document.createElement("div")
+    
+                    imagen.setAttribute('data-bs-toggle', 'modal');
+                    imagen.setAttribute('data-bs-target', '#exampleModal');
+    
+    
+                   imagen.innerHTML = `
+                        
+                    <img class="card-img-top"  src="${image}" alt="">
+                    `
+    
+                    imagen.addEventListener("click",()=>{
+    
+                        const tituloModal =document.querySelector(".modal-title")
+                        tituloModal.innerText=`${name}`
+        
+                        const modalBody =document.querySelector(".modal-body")
+                        modalBody.innerHTML=`
+                        
+                        <img class="card-img-top"  src="${image}" alt="">
+        
+                        `
+                        
+                       })
+    
+                    const titulo = document.createElement("h5")
+                    titulo.classList.add("text-center")
+                    titulo.classList.add("mt-5")
+    
+    
+                    titulo.innerHTML = `
+                    <p>${name}</p>
+                    <p>${race}</p>
+                        
+                    `
+                   
+    
+                    
+                        
+                    cards.appendChild(imagen)
+                    cards.appendChild(titulo)
+                   
+                
+                    col.appendChild(cards)
+                    
+                    main.appendChild(col)
+    
+                    
+                    
+            
+                })
+                .catch((error)=>{
+                    console.log(error)
+                })
+    
+                
+    
+            });
+            
+          
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    
+            
+    
+    
+    
+    }
--- javascript/index.js
+++ javascript/index.js
@@ -1,162 +1,10 @@
 
+import * as cargar from '/javascript/cargaPagina.js'
+
 let page=1
 
-const contenedorButtons = document.querySelector(".contenedor-button-pages")
 
-const btnNext = document.createElement("button")
-btnNext.classList.add("button-pages","next")
-btnNext.innerText="NEXT"
-
-const btnBack = document.createElement("button")
-btnBack.classList.add("button-pages","back")
-btnBack.innerText="BACK"
-
-contenedorButtons.appendChild(btnBack)
-contenedorButtons.appendChild(btnNext)
-
-
-
-function cargarData(page){
-
-
-if (page==1){
-    const back = document.querySelector(".back")
-    back.style.display="none"
-}
-else {
-    const back = document.querySelector(".back")
-    back.style.display="flex"
-}
-
-if (page==6){
-    const next = document.querySelector(".next")
-    next.style.display="none"
-}
-else {
-    const next = document.querySelector(".next")
-    next.style.display="flex"
-}
-
-    
-
-const api = `https://dragonball-api.com/api/characters?page=${page}&limit=10`
-
-
-const main = document.querySelector("#main")
-
-main.innerHTML=`
-    
-
-`
-
-const nube = document.querySelector(".page-nube")
-nube.innerHTML=`${page}`
-
-
-
-
-
-fetch(api)
-    .then((info)=>{
-        return info.json()
-        
-    })
-    .then((response)=>{
-
-        response.items.forEach(element => {
-            const {id} = element
-
-            fetch(`https://dragonball-api.com/api/characters/${id}`)
-            
-            .then((info2)=>{
-                return info2.json()
-            })
-            .then((response2)=>{
-                
-                const {name, image, race} = response2
-
-                const col = document.createElement("div")
-                col.classList.add("col")
-
-                const cards = document.createElement("div")
-                cards.classList.add("card")
-                const imagen = document.createElement("div")
-
-
-               imagen.innerHTML = `
-                    
-                <img class="card-img-top"  src="${image}" alt="">
-                `
-
-                const titulo = document.createElement("h5")
-                titulo.classList.add("text-center")
-                titulo.classList.add("mt-5")
-
-
-                titulo.innerHTML = `
-                <p>${name}</p>
-                <p>${race}</p>
-                    
-                `
-                const mostrar = document.createElement("button")
-                mostrar.classList.add("button-pages","mt-2","mb-3")
-                mostrar.setAttribute('data-bs-toggle', 'modal');
-                mostrar.setAttribute('data-bs-target', '#exampleModal');
-                mostrar.innerText="SHOW"
-
-                
-                    
-                cards.appendChild(imagen)
-                cards.appendChild(titulo)
-                cards.appendChild(mostrar)
-            
-                col.appendChild(cards)
-                
-                main.appendChild(col)
-
-                
-               mostrar.addEventListener("click",()=>{
-
-                const tituloModal =document.querySelector(".modal-title")
-                tituloModal.innerText=`${name}`
-
-                const modalBody =document.querySelector(".modal-body")
-                modalBody.innerHTML=`
-                
-                <img class="card-img-top"  src="${image}" alt="">
-
-                `
-                
-               })
-        
-            })
-            .catch((error)=>{
-                console.log(error)
-            })
-
-            
-
-        });
-        
-      
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
-
-        
-
-
-
-}
-
-
-
-
-
-cargarData(page)
-
-
+cargar.cargarData(page);
 
 
     
@@ -168,7 +16,7 @@ next.addEventListener("click",()=>{
 
     console.log(page)
 
-    cargarData(page)
+    cargar.cargarData(page);
    
 
     
@@ -185,8 +33,7 @@ back.addEventListener("click",()=>{
  
     console.log(page)
 
-    cargarData(page)
-
+    cargar.cargarData(page);
    
 })
 
